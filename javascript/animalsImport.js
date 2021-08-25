96aa9e6ea7a1a34428b63e1b2ef5708cae1af762
JacobGear <imjacobgear@gmail.com>
2021-08-25 19:49:36 +1200

    added method addImage to grab images from local storage and display in html.
    also added method htmlFormatting that gets the json file from local storage and converts it to html.

--- javascript/animalsImport.js
+++ javascript/animalsImport.js
@@ -1,7 +1,58 @@
 let animalsImport = (function(){
     let pub = {};
 
+    function addDropList(html){
+        console.log("addDropList()");
+        html += "<button type=\"button\" class='makeBooking'>Make Booking</button>";
+        return html;
+    }
+
+    function addImage(dog, dogKey, html) {
+        console.log("addImage()");
+        if(dog[dogKey] === "DW-001") {
+            html += "<img class='images' src=\"./images/small.jpg\" alt=\"Small\">";
+        } else if(dog[dogKey] === "DW-002") {
+            html += "<img class='images' src=\"./images/medium.jpg\" alt=\"Medium\">";
+        } else if(dog[dogKey] === "DW-003") {
+            html += "<img class='images' src=\"./images/large.jpg\" alt=\"Large\">";
+        } else if(dog[dogKey] === "DW-004") {
+            html += "<img class='images' src=\"./images/huge.jpg\" alt=\"Huge\">";
+        }
+        return html;
+    }
+
+    function htmlFormatting(dog, dogKey, html) {
+        console.log("htmlFormatting()");
+        let dogField;
+        if(dogKey === "dogName") {
+            dogField = "Dog's Name";
+        } else if(dogKey === "dogId") {
+            dogField = "Dog ID";
+        } else if(dogKey === "dogType") {
+            dogField = "Dog Type";
+        } else if(dogKey === "dogSize") {
+            dogField = "Dog Size";
+        } else if(dogKey === "description") {
+            dogField = "Description";
+        } else if(dogKey === "pricePerHour") {
+            dogField = "Price Per Hour";
+        }
+        if(dogKey !== "pricePerHour" && dogKey !== "dogId") {
+            html += "<p>" + "<strong>" + dogField + ": " + "</strong>" + dog[dogKey] + "</p>";
+        }
+        if(dogKey === "pricePerHour") {
+            html +=  "<p>" + "<strong>" + dogField + ": " + "</strong>" + "$" +
+                "<object class='price'>" + dog[dogKey] + "</object>" + "</p>";
+        }
+        if(dogKey === "dogId") {
+            html += "<p>" + "<strong>" + dogField + ": " + "</strong>" +
+                "<object class='dogId'>" + dog[dogKey] + "</object>" + "</p>";
+        }
+        return html;
+    }
+
     function jsonToHtml(json) {
+        console.log("jsonToHTml()");
         let html = "";
         let keys = Object.keys(json);
         let animals = json[keys];
@@ -11,21 +62,17 @@ let animalsImport = (function(){
             html += "<div class = 'dog'>";
             let dogKeys = Object.keys(dog);
             dogKeys.forEach((dogKey) => {
-                html += "<strong>";
-                html += dogKey;
-                html += "</strong>";
-                html += " : ";
-                html += dog[dogKey];
-                html += "<br />";
+                html = addImage(dog, dogKey, html);
+                html = htmlFormatting(dog, dogKey, html);
             })
+            html = addDropList(html);
             html += "</div>";
             html += "<hr />";
         })
-
-        document.getElementById("products").innerHTML = html;
+        $("#products").html(html);
     }
 
-    function importAnimals() {
+    function importAnimals2() {
         let request = new XMLHttpRequest();
         request.open('GET', "./json/animals.json");
         request.onload = () => {
@@ -33,6 +80,20 @@ let animalsImport = (function(){
             jsonToHtml(result);
         }
         request.send();
+    }
+
+    function importAnimals() {
+        console.log("importAnimals()");
+        $.ajax({
+            type: "GET",
+            async: false,
+            timeout: 30000,
+            url: "./json/animals.json",
+            cache: false,
+            success: function(data) {
+                jsonToHtml(data);
+            }
+        });
     }
 
 
