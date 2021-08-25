f45c0caf2daa9ee64a6134eaf4d1af330e0b2ca9
JacobGear <imjacobgear@gmail.com>
2021-08-25 19:47:48 +1200

    Created addBooking that adds a selected item from a user into sessionstorage

---
+++ javascript/addBooking.js
@@ -0,0 +1,52 @@
+let addBooking = (function(){
+    let pub = {};
+
+    function addToBookings(){
+        console.log("addToBookings()");
+        let parentSection = $(this).parent();
+        let dogId = $(parentSection).find(".dogId").text();
+        let dogPrice = $(parentSection).find(".price").text();
+
+        let item = {};
+        item.price = dogPrice;
+        item.id = dogId;
+
+        updateCookies(item);
+    }
+
+    function duplicateCheck(item){
+        let cart = window.localStorage.getItem("cart");
+        cart = JSON.parse(cart);
+        for(let cartItem of cart){
+            if(item.id === cartItem.id){
+                window.alert("Cannot select the same dog twice!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function updateCookies(item){
+        let cart = window.localStorage.getItem("cart");
+        let cartObj = [];
+        if(cart !== null){
+            cartObj = JSON.parse(cart);
+            if(duplicateCheck(item)) {
+                cartObj.push(item);
+            }
+        } else {
+            cartObj.push(item);
+        }
+        let cartStr = JSON.stringify(cartObj);
+        window.localStorage.setItem("cart", cartStr);
+    }
+
+    pub.setup = function() {
+        $(".makeBooking").click(addToBookings);
+    }
+
+
+    return pub;
+}());
+$(document).ready(addBooking.setup);
+
