dbf46bc502e2bcd4b22bba73fa20e109a1a158cf
JacobGear <imjacobgear@gmail.com>
2021-08-31 15:07:22 +1200

    Added oneach feature that puts features into groups and also binds popups to each marker

--- javascript/map.js
+++ javascript/map.js
@@ -2,8 +2,12 @@ let Map = (function () {
 
     let pub = {};
     let map;
+    let office = L.layerGroup();
+    let walks = L.layerGroup();
+    let parks = L.layerGroup();
 
     function insertGeo() {
+        console.log("insertGeo()");
         $.ajax({
             type: "GET",
             url: "./json/POI.geojson",
@@ -15,6 +19,7 @@ let Map = (function () {
     }
 
     function mapSetup(){
+        console.log("mapSetup()");
         map = L.map('map').setView([-45.875, 170.500], 15);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             { maxZoom: 18,
@@ -25,12 +30,43 @@ let Map = (function () {
     }
 
     function markers(markerLocations){
-        L.geoJSON(markerLocations).addTo(map);
+        console.log("markers()");
+        //L.geoJSON(markerLocations).addTo(map);
+        L.geoJson(markerLocations, {
+            onEachFeature: function (feature, layer) {
+                let content = "Name: " + feature.properties.name + "<br>";
+                content += "Type: " + feature.properties.type + "<br>";
+                layer.bindPopup(content);
+
+                if(feature.properties.type === "office") {
+                    office.addLayer(layer);
+                } else if (feature.properties.type === "walk") {
+                    walks.addLayer(layer);
+                } else {
+                    parks.addLayer(layer);
+                }
+            }
+        })
+        office.addTo(map);
+    }
+
+    function showHide() {
+        console.log("showHide()");
+        if($(this).attr("id") === "showParks") {
+            if($(this).prop('checked')) parks.addTo(map)
+            else parks.remove();
+        } else if($(this).attr("id") === "showWalks") {
+            if($(this).prop('checked')) walks.addTo(map);
+            else walks.remove();
+        }
+
     }
 
     pub.setup = function(){
         mapSetup()
         insertGeo()
+        $("#showWalks").click(showHide);
+        $("#showParks").click(showHide);
     }
 
     return pub;
